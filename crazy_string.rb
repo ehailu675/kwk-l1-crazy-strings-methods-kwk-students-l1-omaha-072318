0f8e70652fae3e1196de1bd10e6ae668c59bca1a
ehailu675 <elisabethahailu@gmail.com>
2018-07-24 18:56:02 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,9 +1,11 @@
 # write your method here
 
 def crazy_strings(first_word, second_word)
+  first_word = crazy_strings
+  
   puts "#{first_word}.reverse.upcase #{second_word}("s","z")"
 end
 
-crazy_strings("Crazy", "Bananas")
+crazy_strings
 
 crazy_strings("hi", "hello")
