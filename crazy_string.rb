8d4ec51f26cecd71409fb8cd3328dc1303ccf6eb
ehailu675 <elisabethahailu@gmail.com>
2018-07-24 18:56:34 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,11 +1,12 @@
 # write your method here
 
-def crazy_strings(first_word, second_word)
-  first_word = crazy_strings
+def crazy_strings
+  # (first_word, second_word)
+#   first_word = crazy_strings
   
-  puts "#{first_word}.reverse.upcase #{second_word}("s","z")"
-end
+#   puts "#{first_word}.reverse.upcase #{second_word}("s","z")"
+# end
 
+# crazy_strings
+end 
 crazy_strings
-
-crazy_strings("hi", "hello")
